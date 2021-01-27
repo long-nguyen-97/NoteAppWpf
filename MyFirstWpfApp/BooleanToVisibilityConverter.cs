43871e8d73472b44262f1560ac65293b2e5a52db
Long Nguyen Hoang <long.nguyenhoang@eastgate-software.com>
2021-01-27 17:20:22 +0700

    init code

---
+++ MyFirstWpfApp/BooleanToVisibilityConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace MyFirstWpfApp
+{
+    public class BooleanToVisibilityConverter : BaseConverter, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter,
+                              CultureInfo culture)
+        {
+            if (value is Boolean)
+            {
+                return ((bool)value) ? Visibility.Visible : Visibility.Collapsed;
+            }
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter,
+                                  CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public abstract class BaseConverter : MarkupExtension
+    {
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+    }
+}
